5089717018e14f5a95b60a75b8229bb36a8a20dd
bustosTrabajo <bustosnoir@gmail.com>
2021-05-25 01:15:17 +0200

    Navigation Drawer

---
+++ app/src/main/java/com/davidbustos/animalesdifunde_v3/Animal.kt
@@ -0,0 +1,11 @@
+package com.davidbustos.animalesdifunde_v3
+
+
+class Animal {
+    var nombre:String?=null
+    var tipo:String?=null
+    var raza:String?=null
+    var usuario_animal:String?=null
+    constructor()
+
+}
--- app/src/main/java/com/davidbustos/animalesdifunde_v3/AnimalesDifundeActivity.kt
+++ app/src/main/java/com/davidbustos/animalesdifunde_v3/AnimalesDifundeActivity.kt
@@ -6,8 +6,12 @@ import android.content.SharedPreferences
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_animales_difunde.*
@@ -15,16 +19,53 @@ import kotlinx.android.synthetic.main.activity_registro.*
 
 class AnimalesDifundeActivity : AppCompatActivity() {
 
+    lateinit var toogle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animales_difunde)
 
         cargarDatos()
 
+
+        drawerLayout=findViewById(R.id.drawerLayout)
+        navigationView=findViewById(R.id.nav_view)
+
+        toogle= ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 ->
+                    //Inflar diferentes fragments o activities relacionados con el item de menú
+                    Toast.makeText(applicationContext,"Perfil de Usuario", Toast.LENGTH_LONG).show()
+
+                R.id.item2 -> Toast.makeText(applicationContext,"Todos mis Animales",Toast.LENGTH_LONG).show()
+
+                R.id.item3 -> Toast.makeText(applicationContext, "Mensajería de Usuario",Toast.LENGTH_LONG).show()
+
+            }
+            true
+        }
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        if(toogle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
     private fun cargarDatos(){
-        val sharedPreferences=getSharedPreferences("sharedPrefs",Context.MODE_PRIVATE)
-        txtNombreUsuario.text=sharedPreferences.getString("usuario","")
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        var usuario=sharedPreferences.getString("usuario","no usuario")
+        txtNombreUsuario.text=usuario
+
     }
     private fun recogerImagen(){
 
